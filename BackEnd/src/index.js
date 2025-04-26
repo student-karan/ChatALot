08000d8065b86f68a0ce00018ef2d1856a10a6cf
Jasmeet Singh <karan9310446410@gmail.com>
2025-04-26 10:46:09 +0530

    project completed

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -25,7 +25,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 
 if (process.env.NODE_ENV === "Production") {
-  app.use(express.static(path.join(__dirname, '../FrontEnd/dist')));
+  app.use(express.static(path.join(__dirname, '../FrontEnd/dist/assets')));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../FrontEnd", "dist" , "index.html"))
