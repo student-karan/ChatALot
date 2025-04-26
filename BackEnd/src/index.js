350b40bc5e22975f30d9a860efc3b4c0e91291b2
Jasmeet Singh <karan9310446410@gmail.com>
2025-04-26 15:40:52 +0530

    project completed

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -3,7 +3,6 @@ if(process.env.NODE_ENV != "Production"){
 dotenv.config();
 }
 import express from "express";
-import fs from "node:fs"
 import { app, server} from "./lib/socket.js";
 import { connectdb } from "./lib/db.js";
 import authRouter from "./routes/auth_Route.js";
@@ -26,25 +25,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 
 if (process.env.NODE_ENV === "Production") {
-  const frontendDistPath = path.join(__dirname, "../FrontEnd/dist");
-  const frontendAssetsPath = path.join(frontendDistPath, "assets");
+  app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
 
-  app.use(express.static(frontendDistPath));
-
-  app.get("*", async (req, res) => {
-    try {
-      const files = await fs.promises.readdir(frontendAssetsPath);
-      const jsFile = files.find(file => file.endsWith(".js"));
-
-      if (jsFile) {
-        res.sendFile(path.join(frontendDistPath, "index.html"));
-      } else {
-        res.status(500).send("Error: Main JavaScript bundle not found.");
-      }
-    } catch (error) {
-      console.error("Error reading assets directory:", error);
-      res.status(500).send("Error serving frontend.");
-    }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FrontEnd", "dist", "index.html"));
   });
 }
 
