6857534ce4048279ba011377af346bd072256e00
Jasmeet Singh <karan9310446410@gmail.com>
2025-04-26 21:46:49 +0530

    project completed

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../FrontEnd","dist","index.html"));
   });
 }
 
