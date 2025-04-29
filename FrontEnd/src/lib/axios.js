d97c0df89ba7cfe65f9861e5ba3c25e3f0faf97c
Jasmeet Singh <karan9310446410@gmail.com>
2025-04-29 18:56:11 +0530

    project completed

--- FrontEnd/src/lib/axios.js
+++ FrontEnd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"? 'http://localhost:5001/api' : '/api',
+    baseURL: import.meta.env.MODE === "development"? 'http://localhost:8080/api' : '/api',
     withCredentials:true,
 })
