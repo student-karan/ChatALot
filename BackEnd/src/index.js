f1196d9a5b27dc03e96bccec940150755756b44f
Jasmeet Singh <karan9310446410@gmail.com>
2025-04-30 17:35:18 +0530

    project completed

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -26,7 +26,7 @@ app.use("/api/message", messageRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../FrontEnd/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
       res.sendFile(path.join(__dirname, "../FrontEnd", "dist", "index.html"));
   });
 }
