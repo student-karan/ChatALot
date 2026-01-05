8a1c45b7fb8b6711bc89a56f1a8fedb3909d0e02
Jasmeet Singh <karan9310446410@gmail.com>
2026-01-05 11:41:13 +0530

    modifying the image upload functionality

--- FrontEnd/src/lib/axios.js
+++ FrontEnd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"? 'http://localhost:8080/api' : '/api',
+    baseURL: import.meta.env.MODE === "development"? 'http://localhost:8080/api' : 'https://chatalot-tjhi.onrender.com/api',
     withCredentials:true,
 })
