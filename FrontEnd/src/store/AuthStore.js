f5e97265c1b3772c7dfa66b5c1fe86f0c37df7e1
Jasmeet Singh <karan9310446410@gmail.com>
2026-01-05 12:03:44 +0530

    modified some cors settings

--- FrontEnd/src/store/AuthStore.js
+++ FrontEnd/src/store/AuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js"
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development"? "http://localhost:8080" : "'https://chatalot-tjhi.onrender.com" ;
+const BASE_URL = import.meta.env.MODE === "development"? "http://localhost:8080" : "https://chatalot-tjhi.onrender.com" ;
 
 export const AuthStore = create((set, get) => ({
     authUser: null,
