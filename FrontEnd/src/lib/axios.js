4be6827a7858bafc618f2d9feae6607f0ba13a1c
Jasmeet Singh <karan9310446410@gmail.com>
2026-01-10 12:37:49 +0530

    some minor changes

--- FrontEnd/src/lib/axios.js
+++ FrontEnd/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"? 'http://localhost:8080/api' : 'https://chatalot-tjhi.onrender.com/api',
+    baseURL: import.meta.env.MODE === "development"? 'http://localhost:8080/api' : 'https://chatalot-ouh0.onrender.com/api',
     withCredentials:true,
 })
--- FrontEnd/src/store/AuthStore.js
+++ FrontEnd/src/store/AuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:8080"
-    : "https://chatalot-tjhi.onrender.com";
+    : "https://chatalot-ouh0.onrender.com";
 
 export const AuthStore = create((set, get) => ({
   authUser: null,
@@ -17,6 +17,7 @@ export const AuthStore = create((set, get) => ({
   online_users: [],
   socket: null,
   checkAuth: async () => {
+    set({isCheckingAuth : true});
     try {
       let res = await axiosInstance.get("/auth/checkAuth");
       set({ authUser: res.data });
